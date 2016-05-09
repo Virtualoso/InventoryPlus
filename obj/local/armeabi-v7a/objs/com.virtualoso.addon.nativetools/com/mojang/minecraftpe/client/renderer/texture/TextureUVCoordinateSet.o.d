60de52250dd5a9654c4f0afcd4c6902d5ccda740
TrinityDevelopers <trinitydevelopers4christ@gmail.com>
2016-05-09 18:27:40 -0500

    finishing tests

--- obj/local/armeabi-v7a/objs/com.virtualoso.addon.nativetools/com/mojang/minecraftpe/client/renderer/texture/TextureUVCoordinateSet.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi-v7a/objs/com.virtualoso.addon.nativetools/com/mojang/minecraftpe/client/renderer/texture/TextureUVCoordinateSet.o: \
- jni/com/mojang/minecraftpe/client/renderer/texture/TextureUVCoordinateSet.cpp \
- jni/com/mojang/minecraftpe/client/renderer/texture/TextureUVCoordinateSet.h
-
-jni/com/mojang/minecraftpe/client/renderer/texture/TextureUVCoordinateSet.h:
