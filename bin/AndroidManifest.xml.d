495957f9b32b25ccfb86033e330845a652dbdbc1
TrinityDevelopers <trinitydevelopers4christ@gmail.com>
2017-06-30 23:10:16 -0500

    remove unnecessary files

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Andrew Miller\Documents\Mod Projects\NativeTools\bin\AndroidManifest.xml : \
-C:\Users\Andrew Miller\Documents\Mod Projects\NativeTools\AndroidManifest.xml \
--- bin/invplus.ap_.d
+++
@@ -1,6 +0,0 @@
-C:\Users\Andrew Miller\Documents\Mod Projects\NativeTools\bin\invplus.ap_ \
- : C:\Users\Andrew Miller\Documents\Mod Projects\NativeTools\res\values\strings.xml \
-C:\Users\Andrew Miller\Documents\Mod Projects\NativeTools\assets\images \
-C:\Users\Andrew Miller\Documents\Mod Projects\NativeTools\assets\images\items-opaque \
-C:\Users\Andrew Miller\Documents\Mod Projects\NativeTools\assets\images\items-opaque\barrier_0.png \
-C:\Users\Andrew Miller\Documents\Mod Projects\NativeTools\bin\AndroidManifest.xml \
