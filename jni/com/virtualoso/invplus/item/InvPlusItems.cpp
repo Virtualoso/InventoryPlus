41ba11fd5caf5a82b1bce40e72f882b9f3cfbccf
TrinityDevelopers <trinitydevelopers4christ@gmail.com>
2017-07-20 19:54:01 -0500

    remove example api

--- jni/com/virtualoso/invplus/item/InvPlusItems.cpp
+++ jni/com/virtualoso/invplus/item/InvPlusItems.cpp
@@ -9,48 +9,6 @@ void InvPlusItems::initItems()
 	//registerItemIds();
 	
 	//barrier = new Barrier("barrier", getNextItemId());
-	CreativeTab* newTab = new CreativeTab(262, 0);
-	newTab->addItem(262, 0);
-	newTab->addItem(43, 0);
-	newTab->addItem(17, 0);
-	newTab->addItem(77, 0);
-	newTab->addItem(55, 0);
-	newTab->addItem(101, 0);
-	newTab->addToTabsList();
-	CreativeTab* newTab2 = new CreativeTab(267, 0);
-	newTab2->addItem(269, 0);
-	newTab2->addItem(49, 0);
-	newTab2->addItem(12, 0);
-	newTab2->addItem(79, 0);
-	newTab2->addItem(52, 0);
-	newTab2->addItem(199, 0);
-	newTab2->addItem(79, 0);
-	newTab2->addItem(52, 0);
-	newTab2->addItem(199, 0);
-	newTab2->addItem(52, 0);
-	newTab2->addItem(199, 0);
-	newTab2->addItem(79, 0);
-	newTab2->addItem(52, 0);
-	newTab2->addItem(199, 0);
-	newTab2->addItem(79, 0);
-	newTab2->addItem(52, 0);
-	newTab2->addItem(199, 0);
-	newTab2->addItem(79, 0);
-	newTab2->addItem(52, 0);
-	newTab2->addItem(199, 0);
-	newTab2->addItem(79, 0);
-	newTab2->addItem(52, 0);
-	newTab2->addItem(199, 0);
-	newTab2->addItem(79, 0);
-	newTab2->addItem(52, 0);
-	newTab2->addItem(199, 0);
-	newTab2->addItem(52, 0);
-	newTab2->addItem(199, 0);
-	newTab2->addItem(79, 0);
-	newTab2->addItem(52, 0);
-	newTab2->addItem(199, 0);
-	newTab2->addItem(79, 0);
-	newTab2->addToTabsList();
 
 }
 
